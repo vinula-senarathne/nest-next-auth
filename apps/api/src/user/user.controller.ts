813e8facb394b8d00ba817dc4ce75935961e27be
Vinula Senarathne <vinula.senarathne@fcodelabs.com>
2025-06-06 12:22:25 +0530

    feat: added API directory

---
+++ apps/api/src/user/user.controller.ts
@@ -0,0 +1,16 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+}
